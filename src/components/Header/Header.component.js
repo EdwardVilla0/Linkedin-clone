c8cc990b683c8f01b60ce4942f9e1201999fd777
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-03 14:47:36 -0800

    installing material UI to the project

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Header.styles.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className="header">
-            <h1>header</h1>
+            <div className="header__left">
+                <img className="header__logo" src="https://dwglogo.com/wp-content/uploads/2020/06/Linkedin_symbol_transparent-2048x1453.png" alt="" />
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+
+            </div>
+            <div className="header__right"></div>
         </div>
     )
 }
