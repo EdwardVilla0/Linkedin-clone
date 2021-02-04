6ebe13c39e596479b1b048ec3eb7afcbf570ff53
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-04 13:33:13 -0800

    styling headerOption

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Header.styles.css';
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import HeaderOption from '../HeaderOption/HeaderOption.component';
 
 function Header() {
     return (
@@ -14,7 +17,10 @@ function Header() {
                 </div>
 
             </div>
-            <div className="header__right"></div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+            </div>
         </div>
     )
 }
