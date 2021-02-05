b18099b634c44ab04cf2eabfb8dd51def59ec8c9
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-05 13:14:05 -0800

    adding styling to css file

--- src/components/Sidebar/Sidebar.component.js
+++ src/components/Sidebar/Sidebar.component.js
@@ -6,10 +6,10 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__top">
-                <img src="" alt="" />
+                <img src="https://images.unsplash.com/photo-1612455897608-ea374d1e87f2?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTd8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
                 <Avatar className="sidebar__avatar" />
-                <h2>testing</h2>
-                <h4>testing more</h4>
+                <h2>Eduardo Villasenor</h2>
+                <h4>eduardovillasenor@gmail.com</h4>
             </div>
             <div className="sidebar__stats">
                 <div className="sidebar__stat">
@@ -20,9 +20,9 @@ function Sidebar() {
                     <p>views on post</p>
                     <p className="sidebar__statNumber">2,455</p>
                 </div>
-                <div className="sidebar__bottom">
-                    <p>Recent</p>
-                </div>
+            </div>
+            <div className="sidebar__bottom">
+                <p>Recent</p>
             </div>
         </div>
     )
