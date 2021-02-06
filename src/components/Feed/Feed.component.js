ad0da9cb0acb4d1173a9642b815260f1ad02beda
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-06 13:08:23 -0800

    created input option component

--- src/components/Feed/Feed.component.js
+++ src/components/Feed/Feed.component.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import CreateIcon from '@material-ui/icons/Create';
+import InputOption from '../InputOption/InputOption.component';
+import ImageIcon from '@material-ui/icons/Image'
 import './Feed.styles.css'
 
 function Feed() {
@@ -12,6 +14,9 @@ function Feed() {
                         <input type="text" />
                         <button type="submit">Send</button>
                     </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
                 </div>
             </div>
         </div>
