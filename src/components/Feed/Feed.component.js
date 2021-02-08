495fbda4e07cf4daec98d6e5f0fe91e4ddcae200
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-08 15:41:23 -0800

    styling the input option

--- src/components/Feed/Feed.component.js
+++ src/components/Feed/Feed.component.js
@@ -2,6 +2,9 @@ import React from 'react'
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from '../InputOption/InputOption.component';
 import ImageIcon from '@material-ui/icons/Image'
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import './Feed.styles.css'
 
 function Feed() {
@@ -17,6 +20,10 @@ function Feed() {
                 </div>
                 <div className="feed__inputOptions">
                     <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Write Article" color="#7FC15E" />
+
                 </div>
             </div>
         </div>
